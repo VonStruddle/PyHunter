0520e5cfb1ddf1004a8c2d4d27d8b4cc098519cf
Durantay Quentin <quentin.durantay@gmail.com>
2017-05-24 18:39:51 +0200

    It's f*cking horrible to upload stuff on PyPI ...

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 setup(
     name='PyHunter',
     packages=['PyHunter'],
-    version='0.1',
+    version='0.2',
     description='An (unofficial) Python wrapper for the Hunter.io API',
     author='Quentin Durantay',
     author_email='quentin.durantay@gmail.com',
     url='https://github.com/VonStruddle/PyHunter',
     download_url='https://github.com/VonStruddle/PyHunter/archive/0.1.tar.gz',
+    install_requires=['requests'],
     keywords=['hunter', 'hunter.io', 'lead generation', 'lead enrichment'],
     classifiers=[],
 )
