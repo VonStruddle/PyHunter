cfc49bf3e1dd2f15c206bd57f70ecd4dafefa433
Durantay Quentin <quentin.durantay@gmail.com>
2017-05-27 22:32:02 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -4,12 +4,22 @@ from setuptools import setup
 setup(
     name='pyhunter',
     packages=['pyhunter'],
-    version='0.2',
+    version='0.1',
     description='An (unofficial) Python wrapper for the Hunter.io API',
     author='Quentin Durantay',
     author_email='quentin.durantay@gmail.com',
     url='https://github.com/VonStruddle/PyHunter',
     install_requires=['requests'],
     keywords=['hunter', 'hunter.io', 'lead generation', 'lead enrichment'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities'
+    ],
 )
